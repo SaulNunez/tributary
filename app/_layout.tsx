471da9847159e2ecf1a590859c7d19bc1da94ef2
Saul Nunez <saul.nunez99@gmail.com>
2026-04-18 14:30:11 -0600

    (WIP) Main screen to show subscribed feed and a screen to import feeds from opm files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,26 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { persistor, store } from '@/store/store';
+import { PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <StoreProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <PaperProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+          </Stack>
+        </PaperProvider>
+      </PersistGate>
+    </StoreProvider>
   );
 }
---
+++ app/import.tsx
@@ -0,0 +1,61 @@
+import { useAppDispatch } from '@/hooks/redux';
+import { processOpml } from '@/opml_processor';
+import { importFromOpml } from '@/store/slices/subscriptions/subscriptionThunks';
+import * as DocumentPicker from 'expo-document-picker';
+import { File } from 'expo-file-system';
+import React from 'react';
+import { Alert, StyleSheet, View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
+
+export default function ImportScreen() {
+    const dispatch = useAppDispatch();
+
+    async function importFeed() {
+        try {
+            // 1. Pick the file
+            const result = await DocumentPicker.getDocumentAsync({
+                type: ['text/xml', 'application/xml'], // Filter for XML files
+                copyToCacheDirectory: true,            // Required to read with FileSystem
+            });
+
+            if (result.canceled) return;
+
+            const { uri } = result.assets[0];
+
+            // 2. Read the file content as a string
+            const file = new File(uri);
+            const fileContent = await file.text();
+            console.log("File content loaded:", fileContent.substring(0, 100)); // Log first 100 chars for debugging
+            const feeds = processOpml(fileContent);
+            dispatch(importFromOpml(feeds));
+            console.log("Imported feeds:", feeds);
+        } catch (error) {
+            Alert.alert("Error", "Could not read the XML file.");
+            throw error;
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text variant="titleMedium" style={styles.text}>
+                Import followed podcasts from another app
+            </Text>
+            <Button mode="contained" onPress={importFeed}>
+                Import
+            </Button>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    text: {
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+});
